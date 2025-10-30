67a5d90870ee178838e927abb3a92100b2a0bd95
Wenchuan Zhao <111623150+WenchuanLiliZhao@users.noreply.github.com>
2025-10-30 16:25:59 +0800

    1

--- app/src/components/enhanced/ClickCounterButton/index.ts
+++
@@ -1,4 +0,0 @@
-export { ClickCounterButton } from './_component'
-export { ClickCounterButton as default } from './_component'
-export type { ClickCounterButtonProps } from './_component'
-
--- app/src/components/enhanced/index.ts
+++
@@ -1,4 +0,0 @@
-// Export all enhanced components
-export { ClickCounterButton } from './ClickCounterButton'
-export type { ClickCounterButtonProps } from './ClickCounterButton'
-
